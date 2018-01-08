d649bdad3c438329177ad8b11948349afdb06375
erikwett <erik.wetterberg@gmail.com>
2018-01-08 08:23:13 +0100

    Added lists
    Added fields and variables

--- syslist.js
+++ syslist.js
@@ -5,7 +5,7 @@ define(["qlik"],
 			".qv-object-syslist .header {font-weight: bold;} " +
 			".qv-object-syslist ul {list-style: none;} " +
 			".qv-object-syslist li {display:inline-block;margin:3px;padding:4px;border:1px solid #ddd;}" +
-			".qv-object-syslist .panel {position: absolute;top: 0;right:0;white-space: pre-wrap;user-select: text;background-color: #ddd;} ";
+			".qv-object-syslist .panel {position: absolute;top: 0;right:0;white-space: pre-wrap;padding: 3px;user-select: text;background-color: #eee;border-left: 2px solid #aaa;} ";
 		var element = document.createElement('style');
 		element.innerHTML = style;
 		document.head.appendChild(element);
@@ -21,7 +21,7 @@ define(["qlik"],
 			var ul = document.createElement('ul');
 			items.forEach(function (i) {
 				var li = document.createElement('li');
-				li.innerHTML = i.qMeta.title;
+				li.innerHTML = i.qName ? i.qName : i.qMeta.title;
 				li.dataset.prop = JSON.stringify(i, null, 4);
 				ul.appendChild(li);
 			});
@@ -41,6 +41,19 @@ define(["qlik"],
 						qDim: "/qDim",
 						qDimInfos: "/qDimInfos"
 					}
+				},
+				qVariableListDef: {
+					qType: "variable",
+					qShowReserved: true,
+					qShowConfig: true
+				},
+				qFieldListDef: {
+					qShowSystem: true,
+					qShowHidden: true,
+					qShowSemantic: true,
+					qShowSrcTables: true,
+					qShowDerivedFields: true,
+					qShowImplicit: true
 				}
 			},
 			support: {
@@ -73,6 +86,9 @@ define(["qlik"],
 					//	title: this.innerHTML
 				})
 				return qlik.Promise.resolve();
+			},
+			resize: function () {
+				// don't need to repaint on resize
 			}
 		};
 
